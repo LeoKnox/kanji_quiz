9eacafba4cc5cbec9ba88500f7eb4c49901b59bd
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-01 00:02:12 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { login } from './login/user.js';
 
 function App() {
+  function formSubmit() {
+    console.log("form submit");
+  }
+  
   return (
     <div className="App">
       <h1>Kanji App</h1>
+      <form onSubmit={formSubmit}>
+        <label>User:</label>
+        <input name="userName" type="text" />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 }

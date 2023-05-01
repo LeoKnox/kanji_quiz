55fa6fef44764262ea1ff20f8b48619f9f52bc39
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-30 23:35:07 -0700

    Add files via upload

--- src/login/user.js
+++ src/login/user.js
@@ -1,7 +1,7 @@
-user = [
+let user = [
     {id:1, userName: "mon", pass: "getsuyobi"},
     {id:1, userName: "thurs", pass: "mokuyobi"}
-]
+];
 
 export default login() {
     console.log("logging in");

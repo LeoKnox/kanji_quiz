7b8fcd23f71e011075a74d6f6bb88e151fdc61b4
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-03 22:49:46 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { useState } from 'react';
 import { login } from './login/user.js';
 
 function App() {
-  const [user, setUser] = useState("a");
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
 
   function upDate(event) {
     if (event.target.name === "userName") {
       setUser(event.target.value);
+    }
+    if (event.target.name === "pass") {
+      setPass(event.target.value);
     }
   }
   function formSubmit() {
@@ -21,6 +25,7 @@ function App() {
       <form onSubmit={formSubmit}>
         <label>User:</label>
         <input name="userName" type="text"  onChange={upDate} value={user} />
+        <input name="pass" type="text"  onChange={upDate} value={pass} />
         <input type="submit" value="Login" />
       </form>
     </div>

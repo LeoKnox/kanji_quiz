b240bee32b0d34ea263462a47d1045ce199dce9a
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-06 22:34:01 -0700

    Add files via upload

--- src/login/user.js
+++ src/login/user.js
@@ -7,12 +7,15 @@ export function login(pass, use) {
     user.find((u, i) => {
         if (u.userName === use) {
             console.log("pass:"+u.pass+":"+pass);
+            /*
             if(u.pass == pass) {
                 //return (user[i]);
                 return ("logged in");
             } else {
                 return("wrong");
             }
+            */
+           u.pass && pass ? console.log("true") : console.log("false");
         } else {
             return("no user");
         }

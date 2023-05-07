475cf1ce584ff4621c57a9427f14437ffa4419c3
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-06 22:36:21 -0700

    Add files via upload

--- src/login/user.js
+++ src/login/user.js
@@ -1,12 +1,13 @@
 let user = [
     {id:1, userName: "mon", pass: "getsuyobi"},
-    {id:1, userName: "thurs", pass: "mokuyobi"}
+    {id:2, userName: "thurs", pass: "mokuyobi"},
+    {id:3, userName: "a", pass: "a"}
 ];
 
 export function login(pass, use) {
     user.find((u, i) => {
         if (u.userName === use) {
-            console.log("pass:"+u.pass+":"+pass);
+            console.log("pass:"+typeof(u.pass)+":"+typeof(pass));
             /*
             if(u.pass == pass) {
                 //return (user[i]);

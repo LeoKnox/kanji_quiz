a46a400e9a2e0dca0d95a7fd6d458985e23d0625
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-07 23:38:43 -0700

    Add files via upload

--- src/login/user.js
+++ src/login/user.js
@@ -15,24 +15,4 @@ export function login(pass, use) {
             console.log("failed password");
         }
     }
-    /*
-    user.find((u, i) => {
-        console.log("1"+use);
-        console.log("2"+u["userName"]);
-        if (u["userName"] === use) {
-            console.log("pass:"+u.pass+":"+pass);
-            /*
-            if(u.pass == pass) {
-                //return (user[i]);
-                return ("logged in");
-            } else {
-                return("wrong");
-            }
-           u.pass && pass ? console.log("true") : console.log("false");
-        } else {
-            return("no user");
-        }
-    })
-        */
-    //return("logging in" + pass + ":" + use);
 }

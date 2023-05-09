827e58cc12bb06eb387f980f299298115a51562d
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-08 21:01:02 -0700

    Add files via upload

--- src/login/password.js
+++ src/login/password.js
@@ -1,7 +1,32 @@
+import { login } from './login/user.js';
+
 function ConfirmPassword() {
+    const [user, setUser] = useState("");
+    const [pass, setPass] = useState("");
+
+    function upDate(event) {
+        if (event.target.name === "userName") {
+        setUser(event.target.value);
+        }
+        if (event.target.name === "pass") {
+        setPass(event.target.value);
+        }
+    }
+
+    function formSubmit(event) {
+        event.preventDefault();
+        console.log("form submit");
+        console.log(login(pass, user));
+    }
+
     return(
         <>
-            <h1>login here</h1>
+            <form onSubmit={formSubmit}>
+                <label>User:</label>
+                <input name="userName" type="text"  onChange={upDate} value={user} />
+                <input name="pass" type="text"  onChange={upDate} value={pass} />
+                <input type="submit" value="Login" />
+            </form>
         </>
     )
 }

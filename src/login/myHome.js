37939fecb68d6a6cdf44ab170a870f9b8725c675
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-09 22:07:01 -0700

    Add files via upload

--- src/login/myHome.js
+++ src/login/myHome.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
+
 function MyHome() {
+    const [currUser, setCurrUser] = useState({
+        userName:"kuruma car",
+        practiceKanji: [1,3,9, 18, 27]
+    })
+
     return (
+        <>
         <h2>My home page</h2>
+        <p>Welcome {currUser.name}</p>
+        {currUser.practiceKanji.map((pk) => {
+            <p>{pk}</p>
+        })}
+        </>
     )
 }
 

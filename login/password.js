8a0bedaff1b1d5c701e7b722b00552df18f581b0
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-12 23:05:26 -0700

    Add files via upload

---
+++ login/password.js
@@ -0,0 +1,43 @@
+import { login } from './user.js';
+import { useState } from 'react';
+
+function ConfirmPassword({param}) {
+    const [user, setUser] = useState("");
+    const [pass, setPass] = useState("");
+
+    function upDate(event) {
+        if (event.target.name === "userName") {
+        setUser(event.target.value);
+        }
+        if (event.target.name === "pass") {
+        setPass(event.target.value);
+        }
+    }
+
+    function formSubmit(event) {
+        param("red");
+        event.preventDefault();
+        console.log("form submit");
+        param(login(pass, user));
+    }
+
+    return(
+        <>
+            <form onSubmit={formSubmit}>
+                <table style={{marginLeft: "auto", marginRight: "auto"}}>
+                    <tr>
+                        <td>User: </td>
+                        <td><input name="userName" type="text"  onChange={upDate} value={user} /></td>
+                    </tr>
+                    <tr>
+                        <td>Password: </td>
+                        <td><input name="pass" type="text"  onChange={upDate} value={pass} /></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Login" />
+            </form>
+        </>
+    )
+}
+
+export default ConfirmPassword;

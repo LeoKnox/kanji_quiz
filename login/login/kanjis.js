f3e5f030be53ab04be77ad36addd07041b440724
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-12 23:08:58 -0700

    Add files via upload

---
+++ login/login/kanjis.js
@@ -0,0 +1,13 @@
+let allKanji = [
+    {id: 1, japanese: "ichi", english: "one"},
+    {id: 3, japanese: "ni", english: "two"},
+    {id: 9, japanese: "san", english: "three"},
+    {id: 18, japanese: "midori", english: "green"},
+    {id: 27, japanese: "akai", english: "red"},
+];
+
+export function findKanji(kanjiId) {
+    console.log(":"+kanjiId);
+    let newKanji = allKanji.find(k => k["id"] === kanjiId);
+    console.log(newKanji);
+}

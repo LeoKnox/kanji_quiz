6f6136fe1a5d7f6061657d89095e4c52b9d8caf6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-05-17 22:56:35 -0700

    Add files via upload

--- src/login/quiz.js
+++ src/login/quiz.js
@@ -4,7 +4,10 @@ function Quiz({practiceKanji}) {
     console.log(practiceKanji.k);
     console.log(practiceKanji.k[Math.floor(Math.random()*practiceKanji.k.length)]);
     return (
+        <>
         <p>Building quiz...</p>
+        <p>{practiceKanji.k}</p>
+        </>
     )
 }
 

6ec692b470d891b38801896a1da116d25bb245a2
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-01 23:26:15 -0700

    Add files via upload

---
+++ src/login/hidekanji.js
@@ -0,0 +1,19 @@
+import { useState } from ' react';
+
+function HideKanji() {
+    let allKanji = [
+        {id: 1, japanese: "ichi", english: "one"},
+        {id: 3, japanese: "ni", english: "two"},
+        {id: 9, japanese: "san", english: "three"},
+        {id: 18, japanese: "midori", english: "green"},
+        {id: 27, japanese: "akai", english: "red"},
+    ];
+
+    return(
+        <>
+            <p>yes, hide kanji</p>
+        </>
+    )
+}
+
+export default HideKanji;

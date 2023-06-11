6512acdadf99449fd1b89b8adec248af3686d299
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-10 23:28:56 -0700

    Add files via upload

--- src/login/displayKanji.js
+++ src/login/displayKanji.js
@@ -2,10 +2,16 @@ import { useState } from 'react';
 
 function DisplayKanji({kanjis}) {
     const [answer, setAnswer] = useState(99);
+    function checkAnswer() {
+        if (answer === 99) {
+            alert("red");
+        }
+    }
     return (
         <>
         <p>{kanjis}</p>
         <p>{answer}</p>
+        <button onClick={checkAnswer}>click</button>
         </>
     )
 }

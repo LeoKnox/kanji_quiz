e0f6962328692150bf60e1141a778e64789381e0
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-17 23:12:17 -0700

    Add files via upload

--- src/login/displayKanji.js
+++ src/login/displayKanji.js
@@ -5,7 +5,8 @@ import alertFun from './kanjiAnswer.js';
 function DisplayKanji({kanjis, allKanji}) {
     //let poolKanji = allKanji.map(x=>({x, r:Math.random() })).sort((a, b) => a.r - b.r).map(a => a.x).slice(0, 3);
     const [poolKanji, setPoolKanji] = useState(allKanji.map(x=>({x, r:Math.random() })).sort((a, b) => a.r - b.r).map(a => a.x).slice(0, 3))
-    const [answer, setAnswer] = useState(poolKanji[Math.floor(Math.random()*poolKanji.length)].english);
+    const [answer, setAnswer] = useState(Math.floor(Math.random()*poolKanji.length));
+    //const [answer, setAnswer] = useState(poolKanji[Math.floor(Math.random()*poolKanji.length)].english);
     console.log(poolKanji);
     console.log(answer);
     const [error, setError] = useState("");

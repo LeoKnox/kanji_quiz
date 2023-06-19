b80b0d55f7a1e6ddbf2f5fff4672653e269ea9c4
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-18 21:49:57 -0700

    Add files via upload

--- src/login/displayKanji.js
+++ src/login/displayKanji.js
@@ -17,7 +17,7 @@ function DisplayKanji({kanjis, allKanji}) {
     }
     
     function newComp() {
-        return(<>{poolKanji.map(t => (<button onClick={() => checkAnswer(t.english)}>{t.japanese}</button>))}</>)
+        return(<>{poolKanji.map(t => (<p><button onClick={() => checkAnswer(t.english)}>{t.japanese}</button></p>))}</>)
     }
     return (
         <>

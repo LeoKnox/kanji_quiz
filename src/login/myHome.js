274f60f59a94062ddb4a285961d22d96662ca44f
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-21 21:10:00 -0700

    Add files via upload

--- src/login/myHome.js
+++ src/login/myHome.js
@@ -21,7 +21,7 @@ function MyHome() {
         <p>Welcome {currUser.userName}</p>
         <button>Practice</button>
         <button onClick={() => setCurrComponent(<QuizKanji />)} value="QuizKanji">Quiz</button>
-        {<currComponent />}
+        <{currComponent} />
         <table style={{marginLeft:"auto", marginRight:"auto"}}>
             <tr>
                 <th>Japanese</th>

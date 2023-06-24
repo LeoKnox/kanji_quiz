f7444e369eee95db0a42a8a87665f8f67e739336
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-23 21:32:33 -0700

    Add files via upload

--- src/login/myHome.js
+++ src/login/myHome.js
@@ -16,13 +16,13 @@ function MyHome() {
         console.log("quiz button clicked " + event.target.value);
         //myComponent = {event.target.value};
         let x = event.target.value;
-        setCurrComponent({x});
+        setCurrComponent(x);
     }
 
     return (testGame ? <>{ testGame }</> :
         <>
         <p>Welcome {currUser.userName}</p>
-    <button onClick={handleClick} value={<NewKanji />}>Practice</button>
+        <button onClick={handleClick} value={<NewKanji />}>Practice</button>
         <button onClick={() => setCurrComponent(<QuizKanji />)} value="QuizKanji">Quiz</button>
         {currComponent}
         <table style={{marginLeft:"auto", marginRight:"auto"}}>

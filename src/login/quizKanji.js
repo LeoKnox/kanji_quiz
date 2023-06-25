4f895dbf017d8e61236415ef176fc58b466bc4d2
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-24 23:14:47 -0700

    Add files via upload

--- src/login/quizKanji.js
+++ src/login/quizKanji.js
@@ -1,10 +1,12 @@
 import StartQuiz from './startQuiz.js';
+import {findKanji} from './kanjis.js';
 
 function QuizKanji({practiceKanji}) {
     console.log("quizKanji " + practiceKanji);
     return (
         <>
         <h3>Quiz Kanji</h3>
+        {findKanji(1)}
         <StartQuiz />
         </>
     )

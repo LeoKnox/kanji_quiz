67e00809acba17e9afe0debbc91d76a8bedf3304
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-26 22:44:30 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -3,7 +3,10 @@ function StartQuiz({practiceKanji}) {
     let answer = practiceKanji.length -1;
     console.log(answer);
     return (
+        <>
         <p>Start new quiz</p>
+        {practiceKanji.map(k => (<p>{k}</p>))}
+        </>
     )
 }
 

faebf9c21fc1d60e2dc085121a64999050d41c00
LeoKnox <LeoKnox@users.noreply.github.com>
2023-06-27 21:16:11 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -3,11 +3,13 @@ function StartQuiz({practiceKanji}) {
     let answer = practiceKanji.length -1;
     console.log(answer);
     function buttonClick() {
+        answer -= 1;
         console.log("on click function");
     }
     return (
         <>
         <p>Start new quiz</p>
+        {answer}
         {practiceKanji.map(k => 
             (<p onClick={buttonClick}>{k}</p>)
         )}

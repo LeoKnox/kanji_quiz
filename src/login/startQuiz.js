85cc87d3123a2bcb0223ad0a79ff6f78adbb6ae4
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-01 23:19:18 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -15,7 +15,7 @@ function StartQuiz({practiceKanji}) {
     return (
         <>
         <p>Start new quiz {myAns}</p>
-        <b>{findKanji(practiceKanji[myAns]).japanese}</b>
+        <b>{findKanji(practiceKanji[myAns].id)}</b>
         {practiceKanji.map(k => 
             (<p onClick={buttonClick} value=
                 {k}>{findKanji(k).english}</p>)

85020e15b8e5f0770af9d746db3deed2b09e8de4
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-02 23:24:29 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -5,7 +5,7 @@ import {useState} from 'react';
 function StartQuiz({practiceKanji}) {
     console.log(practiceKanji);
     let answer = 0;
-    let [myAns, setMyAns] = useState(practiceKanji[Math.floor(Math.random()*practiceKanji.length-1)+1]);
+    let [myAns, setMyAns] = useState(practiceKanji[Math.floor(Math.random()*practiceKanji.length-2)]);
     console.log("t"+myAns);
     let newArray = practiceKanji.slice(1);
     newArray = newArray

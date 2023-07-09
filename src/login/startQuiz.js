e3b318d768b94f6d4acebcb373df8d2cff7a2425
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-08 22:04:51 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -16,9 +16,12 @@ function StartQuiz({practiceKanji}) {
     )
     console.log(Math.floor(Math.random()*practiceKanji.length-2)+1);
     let newArray = practiceKanji.slice(0,4);
+    function addAnswer() {
+        answer++;
+    }
     function buttonClick() {
         console.log(`answer: ${answer}`);
-        answer++;
+        addAnswer();
         console.log(`answer: ${answer}`);
         if (answer >= practiceKanji.length) { alert("done")}
         let temp = practiceKanji.slice(answer, answer+1);

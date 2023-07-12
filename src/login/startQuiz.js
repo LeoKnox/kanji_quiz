bf285909323487958e568f0bb5975f2557a1eb72
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-11 21:40:32 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -3,10 +3,7 @@ import {findKanji} from './twoKanjis.js';
 import {useState} from 'react';
 
 function StartQuiz({practiceKanji}) {
-    console.log(practiceKanji);
-    let [myAns, setMyAns] = useState(findKanji(practiceKanji[0]).english);
     let [answer, incAnswer] = useState(1);
-    //let [myAns, setMyAns] = useState(practiceKanji[Math.floor(Math.random()*practiceKanji.length-1)]);
     let [stateArray, setStateArray] = useState(
         practiceKanji
             .map(x => ({ x, sort: Math.random() }))
@@ -16,33 +13,27 @@ function StartQuiz({practiceKanji}) {
     )
     function buttonClick() {
         incAnswer(answer+1);
-        if (answer >= practiceKanji.length-1) { alert("done")}
-        //let temp = practiceKanji;
-        //console.log(`hold array ${holdArray}`);
+        if (answer >= practiceKanji.length-1) {
+            alert("done")
+
+        }
         let temp = practiceKanji.filter((i, j) => j !== answer);
-        console.log(`answer ${answer}`);
-        console.log(`practice kanji ${practiceKanji}`);
-        //console.log(`hold array ${holdArray}`);
-        console.log(`temp: ${temp}`);
         temp = temp 
             .map(x => ({ x, sort: Math.random() }))
             .sort((i, j) => i.sort - j.sort)
             .map(({x}) => x)
             .slice(0, 4)
         temp[Math.floor(Math.random()*temp.length)] = practiceKanji[answer];
-        console.log(`temp: ${temp}`);
         setStateArray(temp);
-        console.log("on click function");
     }
     return (
         <>
-        <p>Start new quiz {myAns || "t"}</p>
+        <p>Start new quiz {answer || "t"}</p>
         <b>{findKanji(practiceKanji[answer]).japanese}</b>
         {stateArray.map(k => 
             (<p onClick={buttonClick} value=
                 {k}>{findKanji(k).english}</p>)
         )}
-        <b>{findKanji(stateArray[3]).english}</b>
         </>
     )
 }

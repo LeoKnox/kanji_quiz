45910f128ca8d5d4907c9d5f7f398bd8de4e616c
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-18 22:17:12 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -28,14 +28,13 @@ function StartQuiz({practiceKanji}) {
         if (answer >= practiceKanji.length-1) {
             alert("done")
             incAnswer(0);
-            temp = practiceKanji.filter((i, j) => findKanji(practiceKanji[j]).id !== practiceKanji[answer]);
-            temp = temp 
-                .map(x => ({ x, sort: Math.random() }))
-                .sort((i, j) => i.sort - j.sort)
-                .map(({x}) => x)
-                .slice(0, 4)
-            temp[Math.floor(Math.random()*temp.length)] = practiceKanji[answer];
-            setStateArray(temp);
+            setStateArray(
+                practiceKanji
+                    .map(x => ({ x, sort: Math.random() }))
+                    .sort((i, j) => i.sort - j.sort)
+                    .map(({x}) => x)
+                    .slice(0, 4)
+            );
         }
         console.log(`temp ${temp}`);
         setStateArray(temp);

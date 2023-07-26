21333f30f05fe1668b612e82ae943cf1baa2ee65
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-25 23:06:24 -0700

    Add files via upload

--- src/login/twoKanjis.js
+++ src/login/twoKanjis.js
@@ -20,7 +20,8 @@ export function generateQuiz() {
         .sort((i, j) => i.sort - j.sort)
         .map(({x}) => x.id)
         .slice(0, 4)
-    let randAns = x[Math.floor(Math.random()*x.length-1)].id
+    let randAns = x[Math.floor(Math.random()*x.length-1)];
+    console.log(x);
     console.log(`randAns ${randAns}`);
     return (
         {answer:randAns, quiz:x}

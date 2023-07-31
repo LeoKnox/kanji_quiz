7cc75a76b0f4c4bb8cee9f240276954515ba91a6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-07-30 22:11:06 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -3,21 +3,20 @@ import {findKanji, generateQuiz} from './twoKanjis.js';
 import {useState, useEffect} from 'react';
 
 function StartQuiz({practiceKanji}) {
-    let [answer, incAnswer] = useState(1);
+    let [answer, incAnswer] = useState();
     let [stateArray, setStateArray] = useState([])
     useEffect(() => {
         let genQuiz = generateQuiz();
-        console.log(`genQuiz ${JSON.stringify(genQuiz)}`);
-        incAnswer(genQuiz.answer);
+        incAnswer(findKanji(genQuiz.answer).english);
+        console.log(`answer: {genQuiz.answer}`);
         setStateArray(genQuiz.quiz);
-        console.log(`statearr ${stateArray}`)
     },[])
     return (
         <>
         <p>Start new quiz {answer || "t"}</p>
-        {stateArray.map((k) => 
+        {stateArray.map((k) => (
             <p>{findKanji(k).japanese}</p>
-        )}
+        ))}
         </>
     )
 }

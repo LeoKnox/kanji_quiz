c9a06f028e6cc138123b90e6d1bde7ca5a0e8dbd
LeoKnox <LeoKnox@users.noreply.github.com>
2023-08-03 22:27:53 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -5,6 +5,7 @@ import {useState, useEffect} from 'react';
 function StartQuiz({practiceKanji}) {
     let [answer, incAnswer] = useState();
     let [stateArray, setStateArray] = useState([])
+    let kanjis = 0;
     useEffect(() => {
         let genQuiz = generateQuiz();
         incAnswer(findKanji(genQuiz.answer).english);
@@ -20,8 +21,13 @@ function StartQuiz({practiceKanji}) {
             console.log(`newK ${JSON.stringify(newK)}`)
             incAnswer(findKanji(newK.answer).english);
             setStateArray(newK.quiz);
+            kanjis++;
         } else {
             console.log("wrong");
+        }
+        if (kanjis >= 6) {
+            console.log("LOOP COMPLETE")
+            kanjis = 0;
         }
     }
 

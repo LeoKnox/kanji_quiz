aaa757c02e579e322f6f04f1e38bfe06fdd52355
LeoKnox <LeoKnox@users.noreply.github.com>
2023-08-07 21:41:45 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -29,7 +29,7 @@ function StartQuiz({practiceKanji}, changeNewKanji) {
         }
         if (goal  > getTotal()) {
             console.log("LOOP COMPLETE")
-            console.log(typeof(changeNewKanji));
+            console.log(JSON.stringify(changeNewKanji));
             changeNewKanji();
             goal = 0;
         }

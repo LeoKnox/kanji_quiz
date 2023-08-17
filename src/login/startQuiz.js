08dbde4bc5b84824760e75a9bb93ffffc9efa580
LeoKnox <LeoKnox@users.noreply.github.com>
2023-08-16 22:02:10 -0700

    Add files via upload

--- src/login/startQuiz.js
+++ src/login/startQuiz.js
@@ -1,9 +1,7 @@
-//import {findKanji} from './kanjis.js';
 import {getTotal, findKanji, generateQuiz} from './twoKanjis.js';
 import {useState, useEffect} from 'react';
 
 function StartQuiz({cn}) {
-    console.log(`practice kanji ${cn}`)
     let [answer, incAnswer] = useState();
     let [stateArray, setStateArray] = useState([])
     let [goal, setGoal] = useState(1);
@@ -11,28 +9,22 @@ function StartQuiz({cn}) {
     useEffect(() => {
         let genQuiz = generateQuiz();
         incAnswer(findKanji(genQuiz.answer).english);
-        console.log(`answer: {genQuiz.answer}`);
         setStateArray(genQuiz.quiz);
     },[])
 
     const confirmAnswer = (k) => {
-        console.log(`confirmed ${k}`);
         if ((findKanji(k).english) === answer) {
-            console.log("You guessed right!");
             let newK = generateQuiz();
-            console.log(`newK ${typeof(newK)}`)
             incAnswer(findKanji(newK.answer).english);
             setStateArray(newK.quiz);
             setGoal(goal+1);
             setError("");
         } else {
-            console.log("wrong");
             setError("Wrong answer");
         }
         if (goal  >= getTotal()) {
             cn();
         }
-        console.log("kanjis" + goal + ":" + getTotal());
     }
 
     return (

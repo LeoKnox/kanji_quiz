e42b3abeaa63506b8a379015ce4d997f19079e15
LeoKnox <LeoKnox@users.noreply.github.com>
2023-08-18 21:48:33 -0700

    Add files via upload

--- src/login/twoKanjis.js
+++ src/login/twoKanjis.js
@@ -15,15 +15,12 @@ export function getTotal() {
 
 export function findKanji(kanjiId) {
     let newKanji = allKanji.find(k => k["id"] === kanjiId);
-    //HiddenAns(1);
     return(
         newKanji
     );
 }
 
 export function generateQuiz() {
-    //let y = allKanji.filter((a, b) => b !== quizCount);
-    //console.log(`y ${JSON.stringify(y)}`);
     let x = allKanji
         .filter((a, b) => b !== quizCount)
         .map(x => ({ x, sort: Math.random() }))
@@ -31,13 +28,10 @@ export function generateQuiz() {
         .map(({x}) => x.id)
         .slice(0, 4)
     let r=Math.floor(Math.random()*x.length);
-    console.log(`r: ${r}`)
     x[r] = allKanji[quizCount].id;
     let i = Math.floor(Math.random()*x.length);
     let randAns = x[i];
     quizCount++;
-    console.log(quizCount);
-    console.log(`x: ${JSON.stringify(x)}`);
     return (
         {answer:randAns, quiz:x}
     )

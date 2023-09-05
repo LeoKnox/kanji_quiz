22d2d886a3950da4ce07941db5182082c92406a9
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-04 21:47:36 -0700

    Add files via upload

--- src/login/addKanji.js
+++ src/login/addKanji.js
@@ -1,10 +1,12 @@
-import { allMap } from './twoKanjis';
+import { findKanji } from './twoKanjis';
 
-function AddKanji() {
+function AddKanji(myKanji = [1, 3]) {
     return (
         <>
         <h3>Add Kanji</h3>
-        {allMap()}
+        {myKanji.map(k => (
+            <p>{findKanji(k)}</p>
+        ))}
         </>
     )
 }

59aa680def8828c2733ed76d0b02b85e231d23ae
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-06 22:13:09 -0700

    Add files via upload

--- src/login/addKanji.js
+++ src/login/addKanji.js
@@ -11,11 +11,13 @@ function AddKanji({myKanji}) {
         ))}
         <table>
             <tr>
+                <th>Add</th>
                 <th>Japanese</th>
                 <th>English</th>
             </tr>
             {x.map(l => (
                 <tr>
+                    <td><button>Add</button></td>
                     <td>{l.japanese}</td>
                     <td>{l.english}</td>
                 </tr>

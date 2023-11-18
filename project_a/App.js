9468158bce8b5d61b31e51b1580126fadcd25b98
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-17 20:34:37 -0800

    Update App.js

--- project_a/App.js
+++ project_a/App.js
@@ -7,11 +7,18 @@ export default function App() {
     {kanjiId: 2, kanji:"赤", pronounciation:"AKAI", translation:"red"},
     {kanjiId: 1, kanji:"犬", pronounciation:"INU", translation:"dog"}
   ]
-  const MyKanji = createContext();
+  const [currComponent, setCurrComponent] = useState("home");
+
+  function changeComponent(compName="new") {
+    if (compName=="new") {
+      setCurrComponent("new")
+    }
+  }
 
   return (
     <div className="App">
       <h1>Kanji App</h1>
+      {currComponent}
     </div>
   );
 }

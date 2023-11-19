2b752ccc8b8a146d1ea20e41487983f491062903
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-18 22:38:00 -0800

    Update App.js

--- project_a/App.js
+++ project_a/App.js
@@ -30,6 +30,7 @@ export default function App() {
       <h1>Kanji App</h1>
       <button onClick={() => changeComponent("gold")}>change</button>
       <p>{currComponent}</p>
+      {addKanji}
       {kanjiDB.map(k => (
         <p>{k.kanji}</p>
       ))}

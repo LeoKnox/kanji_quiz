0f2ba1d9c2c0c0ec33e53ad9768cf179d198b7bf
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-19 22:33:01 -0800

    Update App.js

--- project_a/App.js
+++ project_a/App.js
@@ -19,6 +19,7 @@ export default function App() {
       //console.log(pronounciation)
       let newKanji = {kanjiId:newId, kanji:kanji, pronounciation:pronounciation, translation:translation};
       console.log(newKanji)
+      kanjiDB.push(newKanji)
     }
     return (
       <form onSubmit={submitKanji}>

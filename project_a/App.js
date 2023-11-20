7529818081b1eddff69407034aa73e4f9f88ffdf
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-19 22:31:08 -0800

    Update App.js

--- project_a/App.js
+++ project_a/App.js
@@ -5,19 +5,20 @@ export default function App() {
   let kanjiDB = [
     {kanjiId: 1, kanji:"一", pronounciation:"ICHI", translation:"one"},
     {kanjiId: 2, kanji:"赤", pronounciation:"AKAI", translation:"red"},
-    {kanjiId: 1, kanji:"犬", pronounciation:"INU", translation:"dog"}
+    {kanjiId: 3, kanji:"犬", pronounciation:"INU", translation:"dog"}
   ]
   const [currComponent, setCurrComponent] = useState("home");
 
   function AddKanji() {
     function submitKanji(event) {
       event.preventDefault()
-      let newId = kanjiDB.length
-      let kanji = event.target.elements.kanji.value
-      let pronounciation = event.target.elements.translation.value
-      let translation = event.target.elements.translation.value
-      console.log(pronounciation)
-      //let newKanji = {kanjiId:newId, kanji:kanji, pronunciation=pronounciation: translation=translation}
+      let newId = kanjiDB.length+1;
+      let kanji = event.target.elements.kanji.value;
+      let pronounciation = event.target.elements.translation.value;
+      let translation = event.target.elements.translation.value;
+      //console.log(pronounciation)
+      let newKanji = {kanjiId:newId, kanji:kanji, pronounciation:pronounciation, translation:translation};
+      console.log(newKanji)
     }
     return (
       <form onSubmit={submitKanji}>

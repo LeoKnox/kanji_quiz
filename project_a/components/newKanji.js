74e3bd8fe5c8f1408206dc95f32a32b852c005cf
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-22 21:31:05 -0800

    Update newKanji.js

--- project_a/components/newKanji.js
+++ project_a/components/newKanji.js
@@ -1,12 +1,12 @@
 function AddKanji({userKanji, setUserKanji}) {
   function submitKanji(event) {
     event.preventDefault()
-    let newId = kanjiDB.length+1;
+    let newId = userKanji.length+1;
     let kanji = event.target.elements.kanji.value;
     let pronounciation = event.target.elements.translation.value;
     let translation = event.target.elements.translation.value;
     let newKanji = {kanjiId:newId, kanji:kanji, pronounciation:pronounciation, translation:translation};
-    setUserKanji(userKanji => [...userKanji, newKanji])
+    //setUserKanji(userKanji => [...userKanji, newKanji])
   }
   return (
     <form onSubmit={submitKanji}>

401f89f8d4dd1130eb806f85dc97d26acad98436
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-24 22:07:34 -0800

    Update App.js

--- project_a/App.js
+++ project_a/App.js
@@ -10,7 +10,7 @@ export default function App() {
   ]
   const [userKanji, setUserKanji] = useState(kanjiDB)
   const [currComponent, setCurrComponent] = useState("home");
-
+  const [updateState, setUpdateState] = useState(-1)
   function EditKanji() {
     return (
       <>

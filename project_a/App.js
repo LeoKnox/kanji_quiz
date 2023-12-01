ac40bace4241dffef5e1bcae6041c9c1cd04bc27
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-30 19:18:20 -0800

    Update App.js

--- project_a/App.js
+++ project_a/App.js
@@ -16,7 +16,7 @@ export default function App() {
       const name=event.target.name
       const value=event.target.value
       const newKanjis = kanjis.map((kan) => (
-        kan.kanjiId === current.kanjiId ? {...kan[name], value} : kan
+        kan.kanjiId === current.kanjiId ? {...kan, name:value} : kan
       ))
       console.log(JSON.stringify(newKanjis))
       setUserKanji(newKanjis)
